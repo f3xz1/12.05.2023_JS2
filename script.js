04e13be5c6ba9f84f71dd7e57f9922d1e828899e
Гагнидзе Давид Владимирович <Gagn_ob80@itstep.edu.az>
2023-05-15 16:27:18 +0400

    15.05.2023

---
+++ script.js
@@ -0,0 +1,70 @@
+function task_1(a,b){
+    if(a>b)
+        return -1;
+    else if (a<b)
+        return 1;
+    
+        return 0;
+}
+
+
+function task_2(a){
+ let res = 1;
+ for (let i = 1; i <= a; i++)
+ {
+    res*=i;
+ }
+ return res;
+}
+
+function task_3(a,b,c){
+
+    return a*100+b*10+c;
+}
+   
+function task_4(a){
+return a**2;
+}
+
+function task_4(a,b){
+    return a*b;
+}
+
+function task_5(a){
+}
+
+function task_6(a,b){
+}
+
+function task_7(a=0,b=0,c=0){
+    return a+':'+b+':'+c;
+}
+
+function task_8(a,b,c){
+    let h = a;
+    let m = b;
+    let s = c;
+
+    m+=h*60;
+    s+=m*60;
+    return s;
+}
+
+
+function task_8(a,b,c){
+    let h = a;
+    let m = b;
+    let s = c;
+
+    m+=h*60;
+    s+=m*60;
+    return s;
+}
+
+function task_9(a,b,c){
+let h = a;
+let m = b;
+let s = c;
+
+
+}
